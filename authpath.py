e6e5c2f9c9fb8ee0c0e96c1b1615137a6dedd2fb
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-05-07 18:58:39 +0200

    Add efficient computation of auth path indices

--- authpath.py
+++ authpath.py
@@ -69,7 +69,7 @@ def pick_leaves(numleaves, height):
     return leaves
 
 # Naive implementation. See authpath.c for fast implementation
-def auth_set(leaves, height):
+def naive_auth_set_indices(leaves, height):
     """Return the authentication set of the given leaves.
 
     The authentication set is the minimal set of nodes necessary to restore the
@@ -96,6 +96,85 @@ def auth_set(leaves, height):
     combined_path = set(accum_auth) - set(accum_path)
 
     return combined_path
+
+def half_dist(l1, l2):
+    """Return half of the geodesic distance between the two leaves l1 and l2.
+
+    This is at the same time the distance from either leaf to their first
+    common parent.
+
+    Keyword arguments:
+    l1, l2 -- the indices of the two leaves
+
+    Returns:
+    The geodesic distance (i.e. the number of edges on the shortes path)
+    from either leaf to the closest common parent node of both leaves.
+    In particular, returns 0 if l1 == l2.
+    """
+    d = 0
+    while l1 != l2:
+        l1 >>= 1
+        l2 >>= 1
+        d += 1
+    return d
+
+def unique(sorted_list):
+    """Return a new list containing all unique elements in their original order
+
+    Keyword arguments:
+    sorted_list -- a list of comparable elements in which all duplicates are
+                   next to one another
+    """
+    res = []
+    last = None
+    for element in sorted_list:
+        if last != element:
+            # Add it to the result list
+            res.append(element)
+            # Skip all following instances of this element
+            last = element
+
+    return res
+
+# Efficient implementation
+def efficient_auth_set_indices(leaves, height):
+    """Return the authentication set of the given leaves.
+
+    The authentication set is the minimal set of nodes necessary to restore the
+    root of the binary hash tree from the given leaves.
+
+    Keyword arguments:
+    leaves -- the set of given leaves as a list of indices in
+              range [0, 2^height-1]
+    height -- the height of the binary hash tree
+
+    Returns:
+    The minimal list of nodes that is needed to restore the root of the binary
+    hash tree from the given leaves. Each node is represented as a tuple
+    consisting of the node's layer, and the node's index within this layer.
+    """
+    sorted_unique_leaves = unique(sorted(leaves))
+    stack = []
+    auth_set = []
+
+    for i in range(0, len(sorted_unique_leaves)):
+        if i < len(sorted_unique_leaves) - 1:
+            d = half_dist(sorted_unique_leaves[i],
+                          sorted_unique_leaves[i+1])
+        else:
+            d = height + 1
+
+        leaf_index = sorted_unique_leaves[i]
+        for h in range(0, d - 1):
+            if len(stack) > 0 and stack[-1] == h:
+                stack.pop()
+            else:
+                # Add that element to the authentication set
+                auth_set.append((h,leaf_index ^ 1))
+            leaf_index >>= 1
+        # Add that height to the stack
+        stack.append(d-1)
+    return auth_set
 
 def trim(set, height, level):
     """Trim the given set to the given level and add all nodes of that level.
@@ -125,18 +204,21 @@ def trim(set, height, level):
     else:
         return set
 
-def sample_authset(numleaves, height):
+def sample_authset(numleaves, height, function):
     """Sample the size of an authentication set.
 
     Keyword arguments:
     numleaves -- the number of given leaves which will be picked randomly
     height    -- the height of the binary hash tree
+    function  -- the function that is used to sample the result. Should be
+                 naive_auth_set or efficient_auth_set, or another function
+                 with the same signature
 
     Returns:
     The length of the generated authentication set.
     """
     leaves = pick_leaves(numleaves, height)
-    authset = auth_set(leaves, height)
+    authset = function(leaves, height)
 
     return len(authset)
 
@@ -170,18 +252,24 @@ def measure(tests, numleaves, height):
     Prints various statistics summarising the measurement.
     """
     # Accumulate the test results
-    results_authsets = [sample_authset(numleaves, height)
-                        for test in range(0, tests)]
+    results_naive_authsets = [sample_authset(numleaves, height, naive_auth_set_indices)
+                              for test in range(0, tests)]
+    results_efficient_authsets = [sample_authset(numleaves, height, efficient_auth_set_indices)
+                                  for test in range(0, tests)]
     results_sphincs  = [sample_sphincs(numleaves, height)
                         for test in range(0, tests)]
     print("# leaves: {}".format(numleaves))
     print("height: {}".format(height))
     print("# samples: {}".format(tests))
     print("Length of separate paths: {}".format(numleaves * height))
-    print("Authentication sets:")
-    print("Min: {}".format(min(results_authsets)))
-    print("Max: {}".format(max(results_authsets)))
-    print("Avg: {}".format(sum(results_authsets) / len(results_authsets)))
+    print("Authentication sets: (naive)")
+    print("Min: {}".format(min(results_naive_authsets)))
+    print("Max: {}".format(max(results_naive_authsets)))
+    print("Avg: {}".format(sum(results_naive_authsets) / len(results_naive_authsets)))
+    print("Authentication sets: (efficient)")
+    print("Min: {}".format(min(results_efficient_authsets)))
+    print("Max: {}".format(max(results_efficient_authsets)))
+    print("Avg: {}".format(sum(results_efficient_authsets) / len(results_efficient_authsets)))
     print("SPHINCS:")
     print("Min: {}".format(min(results_sphincs)))
     print("Max: {}".format(max(results_sphincs)))
