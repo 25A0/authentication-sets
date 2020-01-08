f20b5efb798b00499dfd8a0005c30ab614b98c59
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2020-01-08 13:39:55 +0100

    Add missing script necessary to produce csv data files

---
+++ measure.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import pandas as pd
+import authpath
+
+def measure_dist_sphincs(path):
+    tests = 2**16
+    numleaves = 32
+    height = 16
+    data = [authpath.sample_authset(numleaves, height,
+                                    authpath.efficient_auth_set_indices) \
+            for i in range(0, tests)]
+    df = pd.DataFrame(data, columns=["Size"])
+    df["Height"] = height
+    df["leaves"] = numleaves
+
+    print(df.describe())
+
+    # Export to csv
+    df.to_csv(path)
+
+def measure_num_leaves(path):
+    tests = 2**10
+    height = 16
+    data = [(leaves, \
+             authpath.sample_authset(leaves, height, \
+                                     authpath.efficient_auth_set_indices)) \
+            for i in range(0, tests) \
+            for leaves in {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048}]
+    df = pd.DataFrame(data, columns=["# given leaves","Size"])
+    df["ratio"] = df["Size"] / (df["# given leaves"] * height)
+
+    # Export to csv
+    df.to_csv(path)
+
+if __name__ == '__main__':
+    pass
