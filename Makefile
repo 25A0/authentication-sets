68dadbe5058c9cc56a71b9635189e83fc091488b
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2020-01-08 15:11:37 +0100

    Rename authpath.py -> authset.py

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 %.o: %.c
 	gcc -Wall -o $@ $^
 
-sphincs_distribution.png: authpath.py plot.py
+sphincs_distribution.png: authset.py plot.py
 	python3 -c "import plot; plot.plot_dist_sphincs('$@')"
 
-num_leaves.png: authpath.py plot.py
+num_leaves.png: authset.py plot.py
 	python3 -c "import plot; plot.plot_num_leaves('$@')"
 
-size_distribution.csv: authpath.py measure.py
+size_distribution.csv: authset.py measure.py
 	python3 -c "import measure; measure.measure_dist_sphincs('$@')"
 
-num_leaves.csv: authpath.py measure.py
+num_leaves.csv: authset.py measure.py
 	python3 -c "import measure; measure.measure_num_leaves('$@')"
--- measure.py
+++ measure.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import pandas as pd
-import authpath
+import authset
 
 def measure_dist_sphincs(path):
     tests = 2**16
     numleaves = 32
     height = 16
-    data = [authpath.sample_authset(numleaves, height,
-                                    authpath.efficient_auth_set_indices) \
+    data = [authset.sample_authset(numleaves, height,
+                                    authset.efficient_auth_set_indices) \
             for i in range(0, tests)]
     df = pd.DataFrame(data, columns=["Size"])
     df["Height"] = height
@@ -22,8 +22,8 @@ def measure_num_leaves(path):
     tests = 2**10
     height = 16
     data = [(leaves, \
-             authpath.sample_authset(leaves, height, \
-                                     authpath.efficient_auth_set_indices)) \
+             authset.sample_authset(leaves, height, \
+                                     authset.efficient_auth_set_indices)) \
             for i in range(0, tests) \
             for leaves in {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048}]
     df = pd.DataFrame(data, columns=["# given leaves","Size"])
